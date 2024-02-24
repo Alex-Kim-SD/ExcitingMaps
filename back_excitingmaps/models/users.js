d098533a282e89842fa130e41968c006c0c05e86
alex5 <alex@kimnetics.com>
2024-02-24 11:37:43 -1000

    yes

---
+++ back_excitingmaps/models/users.js
@@ -0,0 +1,5 @@
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  // Add other fields as needed
+});
---
+++ front_excitingmaps/src/components/pages/home.js
@@ -0,0 +1,8 @@
+function Home() {
+
+    return <div>
+<h1>This is the Homepage</h1>
+    </div>
+}
+
+export default Home
